dc55918c1fc297a8d4dc083887d9cd2fa932ba25
Diogo de Lima <diligasi@gmail.com>
2018-02-06 07:08:54 -0200

    Ensure that offer is disabled after creation

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -3,4 +3,14 @@ class Offer < ApplicationRecord
   validates_uniqueness_of :advertiser_name, case_sensitive: false
   validates_length_of :description, maximum: 500
   validates_format_of :url, with: %r{(?:http(s)?://)?[\w.-]+(?:\.[\w.-]+)+[\w\-._~:/?#\[\]@!$&'()*+,;=]+}
+
+  enum state: %i[disabled enabled]
+
+  before_create :set_disabled_state
+
+  private
+
+  def set_disabled_state
+    self.state = :disabled
+  end
 end
