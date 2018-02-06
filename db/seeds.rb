62baa55a3770e6f20a1fc14c08432e4ebf4754cd
Diogo de Lima <diligasi@gmail.com>
2018-02-06 02:27:44 -0200

    Add admin user by seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.find_or_create_by!(email: 'admin@offers.com') do |u|
+  u.password = '123456789'
+  u.password_confirmation = '123456789'
+end
